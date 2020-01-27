893578827b2cce1aaf4d5ebef3d154fae0705615
DheerajKN <kndheeraj0@gmail.com>
2020-01-27 09:28:40 +0530

    Added Inheritance Model to ejs

--- code-snippets/view.js
+++ code-snippets/view.js
@@ -15,12 +15,14 @@ module.exports.addViewToProject = (view, directory) => {
         case 'ejs':
             fetchFile.fetchContent('/template/view.ejs').then((data) =>
                 createFileAndAddContent.createFileWithContent(`${directory}/views/view.ejs`, data))
-            shell.exec('npm i ejs', () => {
+            fetchFile.fetchContent('/template/layout.ejs').then((data) =>
+                createFileAndAddContent.createFileWithContent(`${directory}/views/layout.ejs`, data))
+            shell.exec('npm i ejs express-partials', () => {
                 try {
                     let packageFile = `${directory}/app.js`
                     if (fs.existsSync(packageFile)) {
                         fs.readFile(packageFile, 'utf8', (err, oldContent) => {
-                            let newContent = oldContent.replace(/(.*)express\(\)/g, `const app = express()\napp.set('view engine', 'ejs')\n\napp.get('/index', (req, res) => res.render('view', {title: 'Index'}))`);
+                            let newContent = oldContent.replace(/(.*)express\(\)/g, `const app = express()\napp.set('view engine', 'ejs')\n\nimport partials from 'express-partials'\n// load the express-partials middleware\napp.use(partials());\n\napp.get('/index', (req, res) => res.render('view', {title: 'Index'}))`);
                             fs.writeFile(packageFile, newContent, (err) => {
                                 if (err) throw err;
                             })
